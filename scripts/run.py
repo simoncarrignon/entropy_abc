50ea4bcab8cf161e8dc7a1b943a00c457c866a2f
xrubio <xrubio@gmail.com>
2016-03-22 18:17:16 +0100

    updated sum statistics

--- scripts/run.py
+++ scripts/run.py
@@ -5,17 +5,32 @@ import numpy
 
 def singleRun():
 
-    weightProm = 0.86
-    weightFarming = 0.14
-    alpha = 0.92
-    beta = 0.01
-    harbourBonus = 1.72
+    weightProm = 0.75
+    alpha = 1.57
+    beta = 0.45
+    harbourBonus = 0.68
     cost = entropy.loadCosts('../data/costMatrix.csv')
     sites = '../data/cities_weights.csv'
+    data = entropy.loadHistoricalSites(sites)
 
-    experiment = entropy.Experiment(0,weightProm,weightFarming,alpha,beta,harbourBonus)
-    result = entropy.runEntropy(experiment, cost, sites, True)
-    print('result:',result)
+    experiment = entropy.Experiment(0,weightProm,alpha,beta,harbourBonus)
+    result = entropy.runEntropy(experiment, cost, sites, True)   
+    
+    numTopSites = 25
+    listTopSitesHist = entropy.identifyTopSites(data, numTopSites)
+    listTopSitesSim = entropy.identifyTopSites(result, numTopSites)
+
+    print('top 25 historical sites:')
+    for site in listTopSitesHist:
+        print('\t',site.ident)
+
+    print('top 25 identified sim sites:')
+    count = 0
+    for site in listTopSitesSim:
+        if site in listTopSitesHist:
+            count += 1
+            print('\t',site.ident)
+    print('distance:',1-count/numTopSites)
 
 if __name__ == "__main__":
     singleRun()
