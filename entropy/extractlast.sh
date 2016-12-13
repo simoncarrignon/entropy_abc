fc624b26217ccc92b96566c850fe852520b171db
Simon Carrignon <sc@elisya.org>
2016-12-13 09:51:56 +0100

    more consistent comment

--- entropy/extractlast.sh
+++ entropy/extractlast.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #this bash script shouldn't exist: I used that to avoid spend time knowing how to read csv in python (a 20 min economy) 
+#and is titlte is misleading:it's not "extractlast"but extract "non 0". It's last ONLY if serializatio time = 2
 
 
 awk 'BEGIN{FS=";"}{if(NR > 1 && $1 > 0)print $10 }' $1
